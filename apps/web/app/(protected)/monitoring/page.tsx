5ea954a43e88e1fc3978c4a092b7a21e8ee3a0b6
AyoubLamini <laminiayoub1337@gmail.com>
2026-04-10 14:25:23 +0100

    added Response time section

--- apps/web/app/(protected)/monitoring/page.tsx
+++ apps/web/app/(protected)/monitoring/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 
 // import { ArrowUp } from 'lucide-react';
-import { ShieldCheck } from 'lucide-react';
-import { ShieldAlert } from 'lucide-react';
+import {ShieldCheck, ShieldAlert, ArrowDownToLine, ArrowUpToLine, ArrowDownUp  } from 'lucide-react';
 import { useState } from 'react';
 import { is } from 'zod/v4/locales';
 import { Card, CardHeader, CardContent, CardFooter } from '@/app/Components/ui/card';
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
+
+
+const formatXAxisTick = (value: any): string => {
+  const date = new Date(Date.parse(value)).toString().substring(4, 15);
+  const dayAndMonth = date.substring(0, 6);
+  const year = date.substring(9, 11);
+ const label = dayAndMonth +",'" + year;
+  return `${label}`;
+};
+const formatYAxisTick = (value: any): string => {
+
+  return `${value} ms`;
+};
 
 
 export default function MonitoringPage() {
   const [isApiUp, setIsApiUp] = useState<boolean>(true); 
   const [status, setStatus] = useState<"Up" | "Down">("Up");
+
+  // Mock data
+
   const last24Hours = [
     { time: "00:00", status: "Up" },
     { time: "01:00", status: "Up" },
@@ -38,6 +54,42 @@ export default function MonitoringPage() {
     { time: "22:00", status: "Up" },
     { time: "23:00", status: "Up" },
   ];
+
+
+  // Mock response time data for the last 30 days
+
+  const data = [
+  { date: "2026-03-01", responseTime: 120 },
+  { date: "2026-03-02", responseTime: 98 },
+  { date: "2026-03-03", responseTime: 150 },
+  { date: "2026-03-04", responseTime: 110 },
+  { date: "2026-03-05", responseTime: 135 },
+  { date: "2026-03-06", responseTime: 90 },
+  { date: "2026-03-07", responseTime: 1440 },
+  { date: "2026-03-08", responseTime: 140 },
+  { date: "2026-03-09", responseTime: 105 },
+  { date: "2026-03-10", responseTime: 800 },
+  { date: "2026-03-11", responseTime: 740 },
+  { date: "2026-03-12", responseTime: 95 },
+  { date: "2026-03-13", responseTime: 130 },
+  { date: "2026-03-14", responseTime: 145 },
+  { date: "2026-03-15", responseTime: 100 },
+  { date: "2026-03-16", responseTime: 155 },
+  { date: "2026-03-17", responseTime: 115 },
+  { date: "2026-03-18", responseTime: 140 },
+  { date: "2026-03-19", responseTime: 165 },
+  { date: "2026-03-20", responseTime: 105 },
+  { date: "2026-03-21", responseTime: 125 },
+  { date: "2026-03-22", responseTime: 150 },
+  { date: "2026-03-23", responseTime: 135 },
+  { date: "2026-03-24", responseTime: 460 },
+  { date: "2026-03-25", responseTime: 160 },
+  { date: "2026-03-26", responseTime: 120 },
+  { date: "2026-03-27", responseTime: 322 },
+  { date: "2026-03-28", responseTime: 110 },
+  { date: "2026-03-29", responseTime: 130 },
+  { date: "2026-03-30", responseTime: 150 },
+];
 
   return (
     <div className="p-4 ml-6 mr-6 flex flex-col gap-8">
@@ -101,7 +153,7 @@ export default function MonitoringPage() {
       </div> */}
       </div>
       <div className='flex flex-col w-full bg-gray-800  gap-1  pt-8 p-10 rounded-lg'> 
-        <p className="text-gray-200">Uptime stats</p> 
+        <p className="text-gray-200 font-bold">Uptime stats</p> 
           <div className='flex flex-col lg:flex-row mt-2  '> 
                 <div className="flex flex-col w-full mb-6 lg:mb-0 border-b border-gray-700 lg:border-b-0 lg:border-r lg:border-r-gray-700">
                   <p className="text-gray-200">Last 24 hrs</p> 
@@ -119,12 +171,74 @@ export default function MonitoringPage() {
                   <p className="text-gray-400 text-sm">5 incidents, 1h 12m down</p>
                 </div>
                   <div className="flex flex-col w-full lg:ml-6 ">
-                    <p className="text-gray-200 text-sm bg-land-primary rounded-md pl-2 pr-2 w-fit">Dec 15 22" - May 16 23"</p>
+                    <p className="text-gray-200 text-sm bg-land-primary rounded-md pl-2 pr-2 w-fit">Dec 15 '22 - May 16 '23</p>
                     <h1 className={`text-2xl font-bold ${isApiUp ? "text-primary" : "text-red-500"}`}>.....%</h1>
                     <p className="text-gray-400 text-sm">Upgrade Now</p>
                   </div>
            </div>
-         </div>  
+         </div>
+         <div className="flex flex-col border bg-gray-800 rounded-lg gap-1 pt-8 p-10">
+          <div className='flex flex-row justify-between'>
+              <p className="text-gray-200 font-bold">Response time</p> 
+              <p className="text-gray-200 font-bold  bg-land-primary rounded-md pl-2 pr-2 w-fit">Last 30 days</p> 
+          </div>
+           
+            <div className='flex flex-row w-full'>
+              <div className='flex flex-col w-full'>
+                <div className='h-36' w-full >
+                  <ResponsiveContainer tabIndex={-1} width="100%" height="100%">
+                    <LineChart data={data}>
+                       <CartesianGrid 
+                        stroke="#374151"
+                        vertical={false}   // remove vertical lines
+                      />
+                      <XAxis tabIndex={-1}
+                        axisLine={false}   // remove X axis line
+                        tickLine={false}   // remove tick lines
+                        dataKey="date"
+                        // tickCount={5} // 5 levels (not working)
+                        tickFormatter={formatXAxisTick}
+                        style={{ fontSize: '12px', outline : 'none' }}  
+                      />
+                      <YAxis  tabIndex={-1} 
+                        // tickCount={4} // 3–4 response levels
+                        ticks={[10, 805, 1600]}
+                        tickFormatter={formatYAxisTick}
+                        axisLine={false}   //  remove Y axis line
+                        tickLine={false}   //  remove tick lines
+                        style={{ fontSize: '12px', outline : 'none' }} 
+                      />
+                      {/* <Tooltip /> */}
+                      <Line type="monotone" dataKey="responseTime" strokeWidth={1.5} stroke="#21c45d" dot={false} />
+                    </LineChart>
+                  </ResponsiveContainer>
+                </div>
+                <div className='flex flex-row w-full'>
+                  <div className='w-full'>
+                    <div className='flex flex-row items-center gap-1'>
+                    <ArrowDownUp className='inline text-gray-500'/>
+                    <p className="text-lg font-bold mt-2">101.6 ms</p>
+                    </div>
+                    <span className='text-gray-400 text-sm'>Average</span>
+                  </div>
+                  <div className='w-full'>
+                     <div className='flex flex-row items-center gap-1'>
+                        <ArrowDownToLine className='inline text-primary'/>
+                        <p className="text-lg font-bold mt-2">16.67 ms</p>
+                     </div>
+                    <span className='text-gray-400 text-sm'>Minimum</span>
+                  </div>
+                  <div className='w-full'>
+                      <div className='flex flex-row items-center gap-1'>
+                        <ArrowUpToLine className='inline text-red-500'/>
+                        <p className="text-lg font-bold mt-2">1546.12 ms</p>
+                      </div>
+                    <span className='text-gray-400 text-sm'>Maximum</span>
+                  </div>
+                  </div>
+              </div>
+            </div>
+          </div>  
     </div>
   );
 }
